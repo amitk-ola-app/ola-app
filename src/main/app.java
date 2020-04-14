ee425747a2af93f0dff9eea93ce5986f131fa70e
amitkolurwar <amit.kolurwar25@gmail.com>
2020-04-13 21:11:43 -0400

    updated resolve conflict.

--- src/main/app.java
+++ src/main/app.java
@@ -2,10 +2,7 @@ class{}
 pvsm(string args[])
 {
 system.out.print("Hello World")
-<<<<<<< Updated upstream
 system.out.print("Hello World from remote")
-=======
 system.out.print("Hello World from local")
->>>>>>> Stashed changes
 }
 
