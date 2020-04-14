71a29765065a72202529f848ab5bc7debbb0abdc
amitkolurwar <amit.kolurwar25@gmail.com>
2020-04-13 21:03:29 -0400

    commit stash

--- src/main/app.java
+++ src/main/app.java
@@ -2,6 +2,10 @@ class{}
 pvsm(string args[])
 {
 system.out.print("Hello World")
+<<<<<<< Updated upstream
 system.out.print("Hello World from remote")
+=======
+system.out.print("Hello World from local")
+>>>>>>> Stashed changes
 }
 
